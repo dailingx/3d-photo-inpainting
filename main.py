bf4a737e18501023440af8e2ca2cb3fb2d04b191
dailingx <daixinliang.dxl@alibaba-inc.com>
2023-10-31 17:46:14 +0800

    update path

--- main.py
+++ main.py
@@ -27,6 +27,7 @@ parser = argparse.ArgumentParser()
 parser.add_argument('--config', type=str, default='/root/3d-photo-inpainting/argument.yml',help='Configure of post processing')
 args = parser.parse_args()
 config = yaml.safe_load(open(args.config, 'r'))
+os.environ['DISPLAY']= ':0'
 if config['offscreen_rendering'] is True:
     vispy.use(app='egl')
 os.makedirs(config['mesh_folder'], exist_ok=True)
