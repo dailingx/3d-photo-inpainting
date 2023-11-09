134647029fdf79eac4ab946120fa1b8e4d37bf8b
晚东 <daixinliang.dxl@alibaba-inc.com>
2023-11-09 14:38:37 +0800

    update path

--- bilateral_filtering.py
+++ bilateral_filtering.py
@@ -26,6 +26,8 @@ def sparse_bilateral_filtering(
             window_size = config["filter_size"]
         vis_image = image.copy()
         save_images.append(vis_image)
+        image_width, image_height = vis_image.size
+        print(f'{save_images.mode} {image_width} {image_height}')
         save_depths.append(vis_depth)
         u_over, b_over, l_over, r_over = vis_depth_discontinuity(vis_depth, config, mask=mask)
         vis_image[u_over > 0] = np.array([0, 0, 0])
